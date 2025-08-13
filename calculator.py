ecfced6f0e7579453d169203a2e62220882aed8d
abdul nafih <abdulnafihnafih757@gmail.com>
2025-08-13 12:09:32 +0530

    hfdh

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#basic calculator
+a = 10
+b = 20
+print ("addition of two numbers is:", a + b)
+print ("subtraction of two numbers is:", a - b)
+print ("multiplication of two numbers is:", a * b)
+print ("division of two numbers is:", a / b)
